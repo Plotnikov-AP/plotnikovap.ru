8cda554a2dd00f6f121a5454de9e9a6d726fc6eb
Plotnikov Alexandr <plotnikov-ap@yandex.ru>
2025-01-03 21:20:39 +0300

    edit my-header.blade.php

--- resources/views/components/my-header.blade.php
+++ resources/views/components/my-header.blade.php
@@ -2,9 +2,6 @@
     <h1>Это тестовый сайт Плотникова Александра на Laravel</h1>
     <div class="languageSelect">
         @include('components.language-switch')
-        <!-- <label>Выберите язык</label>
-        <input type="radio" name="lang" value="1" />RU
-        <input type="radio" name="lang" value="2" />EN -->
     </div>
     <div class="clear"></div>
     <div id="menu">
