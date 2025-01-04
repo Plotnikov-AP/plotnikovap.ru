f494c82b30d9262414667c2e09225fd0d4b659e9
Plotnikov Alexandr <plotnikov-ap@yandex.ru>
2025-01-04 21:31:22 +0300

    rn my-menu.blade.php

---
+++ resources/views/components/my-menu.blade.php
@@ -0,0 +1,18 @@
+<div id="menu">
+    <nav>
+        <ul>
+            <li>                                                                         
+                <a href="{{ route('main') }}">{{ __('menu.main') }}</a>                            
+            </li>                                                         
+            <li>                                                                         
+                <a href="#">{{ __('menu.pyatnashki') }}</a>                            
+            </li>
+            <li>                                                                         
+                <a href="#">{{ __('menu.chat') }}</a>                            
+            </li>                                                                       
+            <li>                                                                         
+                <a href="#">{{ __('menu.shop') }}</a>                   
+            </li>                                                                        
+        </ul>
+    </nav>
+</div>      
