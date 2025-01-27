d0dd732325e84ea5241d140d0d205fe259b5008f
Plotnikov Alexandr <plotnikov-ap@yandex.ru>
2025-01-27 13:16:44 +0300

    myjs.js to my.js 2

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -9,7 +9,7 @@
     <script src="{{ asset('/js/jquery.js') }}"></script>             
     <script src="{{ asset('/js/sweetalert2.all.min.js') }}"></script>
     <script src="{{ asset('/js/functions.js') }}"></script> 
-    <script src="{{ asset('/js/js.js') }}"></script>               
+    <script src="{{ asset('/js/my.js') }}"></script>               
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>{{ $title }}</title>
 </head>
